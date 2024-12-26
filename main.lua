1cf1f473082e4d7d6e68f88e35291a42d9b476b6
Cooliokid956 <68075390+Cooliokid956@users.noreply.github.com>
2024-12-26 14:29:25 -0600

    More
    more groundwork for the taunt list

--- main.lua
+++ main.lua
@@ -98,7 +98,6 @@ end
 --     return result
 -- end
 
-local wheelState = 0
 local selectedTaunt
 
 local TWApi = {} -- needs work.,
@@ -282,8 +281,21 @@ local function unit()
     return math.min(djui_hud_get_screen_width(),djui_hud_get_screen_height())
 end
 
----@type Sprite[]
-local sprites = {}
+local function render_text_centered(t, x, y, z)
+    djui_hud_print_text(t, x - djui_hud_measure_text(t) * z/2,  y - 32*z,  z)
+end
+
+local function render_text_centered_interpolated(t, px, py, pz, x, y, z)
+    djui_hud_print_text_interpolated(t, px - djui_hud_measure_text(t) * pz/2, py - 32*pz, pz,
+                                         x - djui_hud_measure_text(t) *  z/2,  y - 32* z,  z)
+end
+
+---@param s Sprite
+function rendertext(s)
+    if not s then return end
+    render_text_centered_interpolated(s.label, s.x - s.xv, s.y - s.yv, s.z - s.zv,
+                                               s.x,        s.y,        s.z       )
+end
 
 ---@type Taunt[]
 local loadout = {}
@@ -315,37 +327,47 @@ hook_event(HOOK_ON_MODS_LOADED, function ()
     end
 end)
 
-local bind = U_JPAD
+---@type Sprite[]
+local listSprites = {}
+local listState = 0
+local listBind = R_JPAD
 
-function checkwheel(m)
-    if m.playerIndex ~= 0 then return end
-    if wheelState == 0 and m.controller.buttonDown & bind ~= 0 and m.action & ACT_FLAG_IDLE ~= 0 then
+---@type Sprite[]
+local wheelSprites = {}
+local wheelState = 0
+local wheelBind = U_JPAD
+
+-- List
+local function check_list(m)
+    if listState == 0 and m.controller.buttonDown & listBind ~= 0 and wheelState == 2 then
+        listState = 1
+    elseif listState == 1 then
+        listState = 2
+    elseif listState == 2 and (m.controller.buttonDown & listBind == 0 or m.action & ACT_FLAG_IDLE == 0) then
+        listState = 3
+    elseif listState == 3 then
+        listState = 4
+    end
+end
+
+local function render_list()
+    
+end
+
+-- Wheel
+local function check_wheel(m)
+    if wheelState == 0 and m.controller.buttonDown & wheelBind ~= 0 and m.action & ACT_FLAG_IDLE ~= 0 then
         wheelState = 1
     elseif wheelState == 1 then
         wheelState = 2
-    elseif wheelState == 2 and (m.controller.buttonDown & bind == 0 or m.action & ACT_FLAG_IDLE == 0) then
+    elseif wheelState == 2 and ((m.controller.buttonDown & wheelBind == 0) or m.action & ACT_FLAG_IDLE == 0) and not (listState == 1 or listState == 2) then
         wheelState = 3
     elseif wheelState == 3 then
         wheelState = 4
     end
 end
 
-function render_text_centered(t, x, y, z)
-    djui_hud_print_text(t, x - djui_hud_measure_text(t) * z/2,  y - 32*z,  z)
-end
-function render_text_centered_interpolated(t, px, py, pz, x, y, z)
-    djui_hud_print_text_interpolated(t, px - djui_hud_measure_text(t) * pz/2, py - 32*pz, pz,
-                                         x - djui_hud_measure_text(t) *  z/2,  y - 32* z,  z)
-end
-
----@param s Sprite
-function rendertext(s)
-    if not s then return end
-    render_text_centered_interpolated(s.label, s.x - s.xv, s.y - s.yv, s.z - s.zv,
-                                               s.x,        s.y,        s.z       )
-end
-
-function renderwheel()
+local function render_wheel()
     djui_hud_set_resolution(RESOLUTION_DJUI)
     djui_hud_set_font(FONT_MENU)
     djui_hud_set_color(255, 255, 255, 255)
@@ -354,23 +376,26 @@ function renderwheel()
     local tauntDist = unit()/7
     local m = gMarioStates[0]
 
+    local cx = (listState == 1 or listState == 2) and (w - h/2) or w/2
+    local cy = h/2
+
     if wheelState == 1 then
         for i, taunt in ipairs(loadout) do
             local sprite = Sprite(taunt.name)
-            table.insert(sprites, sprite)
-            sprite.x  = w/2
-            sprite.y  = h/2
+            table.insert(wheelSprites, sprite)
+            sprite.x  = cx
+            sprite.y  = cy
             sprite.z  = 0.00000001
-            sprite.xv = (math.sin(math.rad((i-1) * 360/#sprites + (math.random()*10))) - math.random()* 10) * unit()/50
-            sprite.yv = (math.cos(math.rad((i-1) * 360/#sprites + (math.random()*10))) - math.random()*-10) * unit()/50
+            sprite.xv = (math.sin(math.rad((i-1) * 360/#wheelSprites + (math.random()*10))) - math.random()* 10) * unit()/50
+            sprite.yv = (math.cos(math.rad((i-1) * 360/#wheelSprites + (math.random()*10))) - math.random()*-10) * unit()/50
             sprite.zv = math.random()*6
         end
     end
     if wheelState == 1 or wheelState == 2 then
         selectedTaunt = nil
-        for i, sprite in ipairs(sprites) do
-            sprite.xv = (sprite.xv + ((w/2 + unit()*0.3 * math.sin((i-1) * 2*math.pi/#sprites)) - sprite.x) * 0.3) * 0.8
-            sprite.yv = (sprite.yv + ((h/2 - unit()*0.3 * math.cos((i-1) * 2*math.pi/#sprites)) - sprite.y) * 0.3) * 0.8
+        for i, sprite in ipairs(wheelSprites) do
+            sprite.xv = (sprite.xv + ((cx + unit()*0.3 * math.sin((i-1) * 2*math.pi/#wheelSprites)) - sprite.x) * 0.3) * 0.8
+            sprite.yv = (sprite.yv + ((cy - unit()*0.3 * math.cos((i-1) * 2*math.pi/#wheelSprites)) - sprite.y) * 0.3) * 0.8
             if not selectedTaunt
                and math.sqrt((sprite.x - djui_hud_get_mouse_x())^2 + (sprite.y - djui_hud_get_mouse_y())^2) <= tauntDist then
                 selectedTaunt = i
@@ -380,59 +405,64 @@ function renderwheel()
             end
             sprite.zv = (sprite.zv + (unit()/800 - sprite.z) * 0.9) * 0.5
         end
-        for i, sprite in ipairs(sprites) do
-            if math.sqrt((sprite.x - (w/2 + (m.controller.extStickX/128 * unit()*0.3)))^2 + (sprite.y - (h/2 - (m.controller.extStickY/128 * unit()*0.3)))^2) <= tauntDist then
+        for i, sprite in ipairs(wheelSprites) do
+            if math.sqrt((sprite.x - (cx + (m.controller.extStickX/128 * unit()*0.3)))^2 + (sprite.y - (cy - (m.controller.extStickY/128 * unit()*0.3)))^2) <= tauntDist then
                 selectedTaunt = i
                 sprite.zv = sprite.zv + unit()*0.001
             end
         end
     end
     if wheelState == 3 then
-        for i, sprite in ipairs(sprites) do
-            sprite.xv = sprite.xv +(math.sin(math.rad((i-1) * 360/#sprites + math.random()*10-5))) * unit()/10
-            sprite.yv = sprite.yv -(math.cos(math.rad((i-1) * 360/#sprites + math.random()*10-5))) * unit()/10
+        for i, sprite in ipairs(wheelSprites) do
+            sprite.xv = sprite.xv +(math.sin(math.rad((i-1) * 360/#wheelSprites + math.random()*10-5))) * unit()/10
+            sprite.yv = sprite.yv -(math.cos(math.rad((i-1) * 360/#wheelSprites + math.random()*10-5))) * unit()/10
             sprite.zv = sprite.zv + math.random()
         end
         if selectedTaunt then
-            if m.action == ACT_IDLE or m.action == ACT_TAUNT then
-                set_mario_action(m, ACT_TAUNT, get_taunt_from_name(sprites[selectedTaunt].label).id)
+            if m.action == ACT_IDLE
+            or m.action == ACT_TAUNT then
+                set_mario_action(m, ACT_TAUNT, get_taunt_from_name(wheelSprites[selectedTaunt].label).id)
             end
         elseif m.action == ACT_TAUNT then
             set_mario_action(m, ACT_IDLE, 0)
         end
     end
     if wheelState == 3 or wheelState == 4 then
-        for i, sprite in ipairs(sprites) do
+        for _, sprite in ipairs(wheelSprites) do
             if sprite.z > 0 then
-                sprite.xv = sprite.xv + (w/2 - sprite.x) * 0.05
-                sprite.yv = sprite.yv + (h/2 - sprite.y) * 0.05
-                sprite.zv = sprite.zv - sprite.z * 0.07
+                sprite.xv,                           sprite.yv,                           sprite.zv =
+                sprite.xv + (cx - sprite.x) * 0.05, sprite.yv + (cy - sprite.y) * 0.05, sprite.zv - sprite.z * 0.07
             end
         end
     end
     if wheelState ~= 0 then
         local done = true
-        render_text_centered("Taunts", w/2, h*0.93, unit()/700)
+        render_text_centered("Taunts", cx, h*0.93, unit()/700)
 
-        for _, sprite in ipairs(sprites) do
+        for _, sprite in ipairs(wheelSprites) do
             if sprite.z + sprite.zv > 0 then
                 done = false
-                sprite.x = sprite.x + sprite.xv
-                sprite.y = sprite.y + sprite.yv
-                sprite.z = sprite.z + sprite.zv
+                sprite.x,             sprite.y,             sprite.z =
+                sprite.x + sprite.xv, sprite.y + sprite.yv, sprite.z + sprite.zv
                 rendertext(sprite)
             end
         end
         if done then
             wheelState = 0
-            sprites = {}
-            save_loadout()
+            wheelSprites = {}
         end
     end
 end
 
-hook_event(HOOK_BEFORE_MARIO_UPDATE,checkwheel)
-hook_event(HOOK_ON_HUD_RENDER,renderwheel)
+hook_event(HOOK_BEFORE_MARIO_UPDATE, function (m)
+    if m.playerIndex ~= 0 then return end
+    check_list(m)
+    check_wheel(m)
+end)
+hook_event(HOOK_ON_HUD_RENDER, function ()
+    render_list()
+    render_wheel()
+end)
 
 function act_taunt(m)
     local taunt = tauntPool[m.actionArg]
